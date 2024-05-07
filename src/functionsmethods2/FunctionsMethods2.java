277a81e2584da0b87e7bb4ed229106f539ef57b6
Camila Martín <camilamartin1403@gmail.com>
2024-05-07 10:32:29 -0300

    subir

---
+++ src/functionsmethods2/FunctionsMethods2.java
@@ -0,0 +1,46 @@
+
+package functionsmethods2;
+
+import java.util.Scanner;
+
+
+public class FunctionsMethods2 {
+
+    public static void main(String[] args) {
+    /*
+    Create a function that validates a number between two numbers.
+    You are going to need two numerical parameters
+    */
+        System.out.println(validateNumber (0,10));
+    
+    public static int validateNumber (int min, int max){
+        Scanner sc = new Scanner (System.in);
+
+        if (min > max){
+        int aux = min;
+        min = max;
+        max = aux;
+    int number;
+    do {
+        System.out.println("Write a number:");
+        number = sc.nextInt ();
+        
+        if (!(number>= 0 && number >= 10));
+        
+        System.out.println("The number "+ number + " it's between "+ min + " and" + max);
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    }
+    
+}
